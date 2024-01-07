9bf44695c62abf53a14ba78be0a302dabff95a57
Ivan <45673809+1v4n1v4n@users.noreply.github.com>
2024-01-06 22:41:17 -0500

    Add files via upload

---
+++ router.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { UserSettings } from "./pages/UserSettings";
+import { Categories } from "./pages/Categories";
+import { NotFound } from "./pages/NotFound";
+import { AddTask } from "./pages/AddTask";
+import { ImportExport } from "./pages/ImportExport";
+import { Home } from "./pages/Home";
+import { TaskDetails } from "./pages/TaskDetails";
+import { SharePage } from "./pages/Share";
+
+export const AppRouter = (): JSX.Element => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/task/:id" element={<TaskDetails />} />
+      <Route path="/share" element={<SharePage />} />
+      <Route path="/add" element={<AddTask />} />
+      <Route path="/user" element={<UserSettings />} />
+      <Route path="/import-export" element={<ImportExport />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
